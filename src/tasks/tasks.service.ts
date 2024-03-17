c5e017297e40562ce4dca42a05840d31982e2e27
amirshayanyian <wahtamir12@gmail.com>
2024-03-17 14:19:23 +0330

    Craeted a 'createTask' service method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './tasks.model';
+import { Task, TaskStatus } from './tasks.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
-  getAllTasks() {
+
+  getAllTasks(): Task[] {
     return this.tasks;
+  }
+  createTasks(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.Open,
+    };
+    this.tasks.push(task);
+    return task;
   }
 }
