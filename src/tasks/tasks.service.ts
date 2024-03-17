d2cb9eae757087f5e383f177f5787b80610fd32f
amirshayanyian <wahtamir12@gmail.com>
2024-03-17 13:59:26 +0330

    Created a tasks list variable to get the tasks from the controller

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks = ['HI', 'sad'];
+  getAllTasks() {
+    return this.tasks;
+  }
+}
