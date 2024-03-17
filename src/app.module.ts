3d294642ce1643b8d35982d49d746c1bd2e34fa7
amirshayanyian <wahtamir12@gmail.com>
2024-03-17 10:00:10 +0330

    Craeted tasks Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TaskscontrollerController } from './taskscontroller/taskscontroller.controller';
 
 @Module({
   imports: [TasksModule],
+  controllers: [TaskscontrollerController],
 })
 export class AppModule {}
