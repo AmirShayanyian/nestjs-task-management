d3a9ec44006b87abd7fcb06809d7b2dd1f0598e2
amirshayanyian <wahtamir12@gmail.com>
2024-03-19 17:07:19 +0330

    Created a dto for sign In and a route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credentials.dto';
+import { UserSignInDto } from './dto/user-signin.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('signup')
+  @Post('/signup')
   signUp(@Body() userDto: AuthCredentialDto): Promise<void> {
     return this.authService.createUser(userDto);
+  }
+
+  @Post('/signin')
+  signIn(@Body() signInDto: UserSignInDto): Promise<string> {
+    return this.authService.signIn(signInDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,14 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { UserRepository } from './users.repository';
 import { AuthCredentialDto } from './dto/auth-credentials.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import * as bcrypt from 'bcrypt';
+import { UserSignInDto } from './dto/user-signin.dto';
 
 @Injectable()
 export class AuthService {
@@ -22,8 +24,8 @@ export class AuthService {
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    console.log('salt: ', salt);
-    console.log('hashedPassword: ', hashedPassword);
+    //  console.log('salt: ', salt);
+    // console.log('hashedPassword: ', hashedPassword);
 
     const user = this.userRepository.create({
       username,
@@ -44,5 +46,16 @@ export class AuthService {
 
   async signUp(userDto: AuthCredentialDto): Promise<void> {
     return this.createUser(userDto);
+  }
+
+  async signIn(signInDto: UserSignInDto): Promise<string> {
+    const { username, password } = signInDto;
+    const user = await this.userRepository.findOne({ where: { username } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'Success';
+    } else {
+      throw new UnauthorizedException('Please check the login credentials.');
+    }
   }
 }
