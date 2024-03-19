d9ca15afc2370ec776a5823c6b470664c2208c13
amirshayanyian <wahtamir12@gmail.com>
2024-03-19 16:14:22 +0330

    Added a feature to error against duplicate username

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { UserRepository } from './users.repository';
 import { AuthCredentialDto } from './dto/auth-credentials.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    @InjectRepository(User)
+    private userRepository: UserRepository,
+  ) {}
 
   async createUser(userDto: AuthCredentialDto): Promise<void> {
     const { username, password } = userDto;
     const user = this.userRepository.create({
-      username: username,
-      password: password,
+      username,
+      password,
     });
-    await this.userRepository.save(user);
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Duplicate username!');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 
   async signUp(userDto: AuthCredentialDto): Promise<void> {
--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
 export class AuthCredentialDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/, {
+    message: 'password is weak.',
+  })
   password: string;
 }
